6eadde1608d1c560cd47ce77a415a70077f81488
Stakhmich Karina <karinastahmic@gmail.com>
2025-10-21 12:49:57 +0300

    1111

--- lab3_302.py
+++ lab3_302.py
@@ -1,6 +1,11 @@
 a = int(input("Enter a: "))
 b = int(input("Enter b: "))
 
-for i in range(a, b + 1):
-    if i % 2 != 0:
-        print(i, end=' ')
+if a <= b:
+    for i in range(a, b + 1):
+        if i % 2 != 0:
+            print(i, end=' ')
+else:
+    for i in range(a, b - 1, -1):
+        if i % 2 != 0:
+            print(i, end=' ')
